a036088318422716e1b220c2c6a27d75e6e5acc2
Selvakumar Esra <selva@typesafe.me>
2021-03-21 09:53:22 +0800

    changed the query logic to between

--- src/main/java/io/selvaesra/eventful/repositories/EventRepository.java
+++ src/main/java/io/selvaesra/eventful/repositories/EventRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
-    @Query(value="SELECT e.event_surrogate_id,c.category_surrogate_id,e.venue_surrogate_id,e.event_name,e.short_description,e.from_datetime,e.to_datetime,photo_url,c.category_name ,v.coordinate, v.location, v.venue_name from EVENT e, CATEGORY c, VENUE v where e.category_surrogate_id = c.category_surrogate_id and e.venue_surrogate_id = v.venue_surrogate_id and v.location=:location and c.category_name=:category and DATE_FORMAT(e.from_datetime,'%Y-%m-%d')=:fromDateTime and DATE_FORMAT(e.to_datetime,'%Y-%m-%d')=:toDateTime",nativeQuery=true)
+    @Query(value="SELECT e.event_surrogate_id,c.category_surrogate_id,e.venue_surrogate_id,e.event_name,e.short_description,e.from_datetime,e.to_datetime,photo_url,c.category_name ,v.coordinate, v.location, v.venue_name from EVENT e, CATEGORY c, VENUE v where e.category_surrogate_id = c.category_surrogate_id and e.venue_surrogate_id = v.venue_surrogate_id and v.location=:location and c.category_name=:category and DATE_FORMAT(e.from_datetime,'%Y-%m-%d') BETWEEN :fromDateTime AND :toDateTime",nativeQuery=true)
     List<Event> findByLocationAndCategoryAndFromDateTime(@Param("location") String location,
                                                          @Param("category") String category,
                                                          @Param("fromDateTime") String fromDateTime,
